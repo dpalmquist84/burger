4b3f4c5d7aebb94bc918bc76af8ead23a035fbac
David Palmquist <dpalmquist84@gmail.com>
2017-10-02 10:43:07 -0400

    changed the layout

---
+++ app/models/routes/api-routes.js
@@ -0,0 +1,45 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var Burger = require("../models/burger.js");
+
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // Get all chirps
+  app.get("/api/all", function(req, res) {
+
+    // Finding all Chirps, and then returning them to the user as JSON.
+    // Sequelize queries are aynchronous, which helps with percieved speed.
+    // If we want something to be guaranteed to happen after the query, we'll use
+    // the .then function
+    Burger.findAll({}).then(function(results) {
+      // results are available to us inside the .then
+      res.json(results);
+    });
+
+  });
+
+  // Add a chirp
+  app.post("/api/new", function(req, res) {
+
+    console.log("Burger Data:");
+    console.log(req.body);
+
+    Burger.create({
+      burger: req.body.burger,
+      taste: req.body.taste,
+      created_at: req.body.created_at
+    }).then(function(results) {
+      // `results` here would be the newly created chirp
+      res.end();
+    });
+
+  });
+
+};
--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,31 @@
-orm = require("./config/orm.js")
+// Dependencies
+// =============================================================
 
+// This may be confusing but here Sequelize (capital) references the standard library
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references our connection to the DB.
+var sequelize = require("../config/connection.js");
 
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').focus()
-  })
+// Creates a "Chirp" model that matches up with DB
+var Burger = sequelize.define("burger", {
+  burger: {
+    type: Sequelize.STRING
+  },
+  is_devoured: {
+    type: Sequelize.BOOLEAN
+  },
+  taste: {
+    type: Sequelize.STRING
+  },
+  created_at: {
+    type: Sequelize.DATE
+  }
+}, {
+  timestamps: false
+});
 
-module.exports = orm;
+// Syncs with DB
+Burger.sync();
+
+// Makes the Burger Model available for other files (will also create a table)
+module.exports = Burger;
